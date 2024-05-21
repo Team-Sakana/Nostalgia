7d038eceb8f1672f2014b226db5bc97f32b89a8a
dlwlgns268 <xyz098711@gmail.com>
2024-05-21 18:40:49 +0900

    a

--- dokidokiCode_fish/Assets/BBang/Timer.cs
+++ dokidokiCode_fish/Assets/BBang/Timer.cs
@@ -1,36 +1,37 @@
 using System.Collections;
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.Serialization;
 using UnityEngine.UI;
 
-namespace BBang
+public class Timer : MonoBehaviour
 {
-    public class Timer : MonoBehaviour
-    {
-        [FormerlySerializedAs("TimeText")] public Text timeText;
-
-
-        private void Start()
-        {
-            StartCoroutine(Wait(60f));
-        }
-
-        private static IEnumerator Wait(float time)
-        {
-            var timeElapsed = 0f;
-            while(timeElapsed<time)
-            {
-                timeElapsed += UnityEngine.Time.deltaTime;
-                yield return null;
-            }
-            SceneManager.LoadScene("New Scene");
-        }
-        // Update is called once per frame
-        private void Update()
-        {
-       
-        }
+    public TMP_Text TimeText;
     
+    
+    void Start()
+    {
+        StartCoroutine(wait(60f));
     }
+
+    IEnumerator wait(float Time)
+    {
+        float elepsedtime = 0f;
+        while(elepsedtime<Time)
+        {
+            elepsedtime += UnityEngine.Time.deltaTime;
+            TimeText.text = (Time - elepsedtime).ToString("0.00");
+            yield return null;
+        }
+        SceneManager.LoadScene("New Scene");
+        yield break;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+   
+    
 }
--- dokidokiCode_fish/Assets/BBang/create customer.cs
+++ dokidokiCode_fish/Assets/BBang/create customer.cs
@@ -1,194 +1,187 @@
+using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Dynamic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
-using UnityEngine.Serialization;
 using UnityEngine.UI;
 using Random = UnityEngine.Random;
 
-namespace BBang
+public class createcustomer : MonoBehaviour
 {
-    public class CreateCustomer : MonoBehaviour
+    public TMP_Text scoretext;
+    public bool breadsubmit = false;
+    public bool eotk = false;
+    private int bread1 = 0;
+    private int bread2 = 0;
+    private int bread3 = 0;
+    private int score = 0;
+
+    private int B1 = 0;
+    private int B2 = 0;
+    private int B3 = 0;
+    public static int b1g = 0;
+    public static int b2g = 0;
+    public static int b3g = 0;
+    [SerializeField]
+    public GameObject Customer;
+
+    public GameObject Text;
+
+    public GameObject Bread1;
+    public GameObject Bread2;
+    public GameObject Bread3;
+
+    public Button Button;
+    public Button Button1;
+    public Button Button2;
+    // Start is called before the first frame update
+
+    
+    
+
+    void Start()
     {
-        [FormerlySerializedAs("eotk")] public bool endOfTalk = false;
-        [FormerlySerializedAs("bread1")] public int bread1Random = 0;
-        [FormerlySerializedAs("bread2")] public int bread2Random = 0;
-        [FormerlySerializedAs("bread3")] public int bread3Random = 0;
-        public int score = 0;
-
-        [FormerlySerializedAs("B1")] public int b1 = 0;
-        [FormerlySerializedAs("B2")] public int b2 = 0;
-        [FormerlySerializedAs("B3")] public int b3 = 0;
-        public static int B1G = 0;
-        public static int B2G = 0;
-        public static int B3G = 0;
-        [FormerlySerializedAs("Customer")] [SerializeField]
-        public GameObject customer;
-
-        [FormerlySerializedAs("Text")] public GameObject text;
-
-        [FormerlySerializedAs("Bread1")] public GameObject bread1;
-        [FormerlySerializedAs("Bread2")] public GameObject bread2;
-        [FormerlySerializedAs("Bread3")] public GameObject bread3;
-
-        [FormerlySerializedAs("Button")] public Button button;
-        [FormerlySerializedAs("Button1")] public Button button1;
-        [FormerlySerializedAs("Button2")] public Button button2;
-
-        public bool scoreReceived;
-        // Start is called before the first frame update
-
-
-        private void Start()
-        {
-            Create();
-            StartCoroutine(Delay(8f));
+        Create();
+        StartCoroutine(Delay(4f));
+        scoretext.text = (score).ToString();
         
-            button.onClick.AddListener(ClickB1);
-            button1.onClick.AddListener(ClickB2);
-            button2.onClick.AddListener(ClickB3);
+        Button.onClick.AddListener(clickb1);
+        Button1.onClick.AddListener(clickb2);
+        Button2.onClick.AddListener(clickb3);
+    }
+
+    IEnumerator Delay(float time)
+    {
+        eotk = false;
+        float elepsedtime = 0f;
+        while(elepsedtime<time)
+        {
+            elepsedtime += UnityEngine.Time.deltaTime;
+            yield return null;
         }
+        
+        yield return new WaitForSecondsRealtime(2f);
+        Create();
+        StartCoroutine(Delay(time));
+        yield break;
 
-        private IEnumerator Delay(float time)
+    }
+    private void Create()
+    {
+        breadsubmit = false;
+        B1 = 0;
+        B2 = 0;
+        B3 = 0;
+        b1g = 0;
+        b2g = 0;
+        b3g = 0;
+        bread1 = Random.Range(1, 4);
+        bread2 = Random.Range(1, 4);
+        bread3 = Random.Range(1, 4);
+        var clonec = Instantiate(Customer);
+        var clonet = Instantiate(Text);
+        clonec.transform.position = new Vector3(-4.5f, 0.75f, 0);
+        clonet.transform.position = new Vector3(3.35f, 1.4f, 0f);
+        if (bread1 == 1)
         {
-            endOfTalk = false;
-            var timeElapsed = 0f;
-            while(timeElapsed<time)
-            {
-                timeElapsed += Time.deltaTime;
-                yield return null;
-            }
-        
-            yield return new WaitForSecondsRealtime(2f);
-            Create();
-            StartCoroutine(Delay(4f));
+            var cloneb1 = Instantiate(Bread1);
+            cloneb1.transform.position = new Vector3(0f, 1.6f, 0f);
+            Destroy(cloneb1,4f);
+            b1g++;
         }
-        private void Create()
+        else if (bread1 == 2)
         {
-            b1 = 0;
-            b2 = 0;
-            b3 = 0;
-            B1G = 0;
-            B2G = 0;
-            B3G = 0;
-            scoreReceived = false;
-            bread1Random = Random.Range(1, 4);
-            bread2Random = Random.Range(1, 4);
-            bread3Random = Random.Range(1, 4);
-            var cloneC = Instantiate(customer);
-            var cloneT = Instantiate(text);
-            cloneC.transform.position = new Vector3(-4, 2, 0);
-            cloneT.transform.position = new Vector3(3.8f, 2.75f, 0f);
-            switch (bread1Random)
-            {
-                case 1:
-                {
-                    var cloneB1 = Instantiate(bread1);
-                    cloneB1.transform.position = new Vector3(0.9f, 2.9f, 0f);
-                    Destroy(cloneB1,4f);
-                    B1G++;
-                    break;
-                }
-                case 2:
-                {
-                    var cloneB2 = Instantiate(bread2);
-                    cloneB2.transform.position = new Vector3(0.8f, 2.9f, 0f);
-                    Destroy(cloneB2,4f);
-                    B2G++;
-                    break;
-                }
-                case 3:
-                {
-                    var cloneB3 = Instantiate(bread3);
-                    cloneB3.transform.position = new Vector3(0.95f, 2.7f, 0f);
-                    Destroy(cloneB3,4f);
-                    B3G++;
-                    break;
-                }
-            }
-        
-            switch (bread2Random)
-            {
-                case 1:
-                {
-                    var cloneB1 = Instantiate(bread1);
-                    cloneB1.transform.position = new Vector3(3.7f, 2.9f, 0f);
-                    Destroy(cloneB1,4f);
-                    B1G++;
-                    break;
-                }
-                case 2:
-                {
-                    var cloneB2 = Instantiate(bread2);
-                    cloneB2.transform.position = new Vector3(3.6f, 2.9f, 0f);
-                    Destroy(cloneB2,4f);
-                    B2G++;
-                    break;
-                }
-                case 3:
-                {
-                    var cloneB3 = Instantiate(bread3);
-                    cloneB3.transform.position = new Vector3(3.75f, 2.7f, 0f);
-                    Destroy(cloneB3,4f);
-                    B3G++;
-                    break;
-                }
-            }
-        
-            switch (bread3Random)
-            {
-                case 1:
-                {
-                    var cloneB1 = Instantiate(bread1);
-                    cloneB1.transform.position = new Vector3(6.5f, 2.9f, 0f);
-                    Destroy(cloneB1,4f);
-                    B1G++;
-                    break;
-                }
-                case 2:
-                {
-                    var cloneB2 = Instantiate(bread2);
-                    cloneB2.transform.position = new Vector3(6.4f, 2.9f, 0f);
-                    Destroy(cloneB2,4f);
-                    B2G++;
-                    break;
-                }
-                case 3:
-                {
-                    var cloneB3 = Instantiate(bread3);
-                    cloneB3.transform.position = new Vector3(6.45f, 2.7f, 0f);
-                    Destroy(cloneB3,4f);
-                    B3G++;
-                    break;
-                }
-            }
-            Destroy(cloneC, 4f);
-            Destroy(cloneT, 4f);
-        
-            endOfTalk = true;
+            var cloneb2 = Instantiate(Bread2);
+            cloneb2.transform.position = new Vector3(0f, 1.6f, 0f);
+            Destroy(cloneb2,4f);
+            b2g++;
         }
+        else if (bread1 == 3)
+        {
+            var cloneb3 = Instantiate(Bread3);
+            cloneb3.transform.position = new Vector3(0f, 1.6f, 0f);
+            Destroy(cloneb3,4f);
+            b3g++;
+        }
+        
+        if (bread2 == 1)
+        {
+            var cloneb1 = Instantiate(Bread1);
+            cloneb1.transform.position = new Vector3(3.2f, 1.6f, 0f);
+            Destroy(cloneb1,4f);
+            b1g++;
+        }
+        else if (bread2 == 2)
+        {
+            var cloneb2 = Instantiate(Bread2);
+            cloneb2.transform.position = new Vector3(3.2f, 1.6f, 0f);
+            Destroy(cloneb2,4f);
+            b2g++;
+        }
+        else if (bread2 == 3)
+        {
+            var cloneb3 = Instantiate(Bread3);
+            cloneb3.transform.position = new Vector3(3.2f, 1.6f, 0f);
+            Destroy(cloneb3,4f);
+            b3g++;
+        }
+        
+        if (bread3 == 1)
+        {
+            var cloneb1 = Instantiate(Bread1);
+            cloneb1.transform.position = new Vector3(6.4f, 1.6f, 0f);
+            Destroy(cloneb1,4f);
+            b1g++;
+        }
+        else if (bread3 == 2)
+        {
+            var cloneb2 = Instantiate(Bread2);
+            cloneb2.transform.position = new Vector3(6.4f, 1.6f, 0f);
+            Destroy(cloneb2,4f);
+            b2g++;
+        }
+        else if (bread3 == 3)
+        {
+            var cloneb3 = Instantiate(Bread3);
+            cloneb3.transform.position = new Vector3(6.4f, 1.6f, 0f);
+            Destroy(cloneb3,4f);
+            b3g++;
+        }
+            Destroy(clonec, 4f);
+            Destroy(clonet, 4f);
+        
+        eotk = true;
+    }
 
-        private void Update()
+    void Update ()
+    {
+          
+        if (b1g == B1 && b2g == B2 && b3g == B3 && !breadsubmit)
         {
-            if (B1G != b1 || B2G != b2 || B3G != b3 || scoreReceived) return;
-            scoreReceived = true;
             score += 1000;
-            b1 = 0;
-            b2 = 0;
-            b3 = 0;
+            B1 = 0;
+            B2 = 0;
+            B3 = 0;
+            breadsubmit = true;
+            scoretext.text = (score).ToString();
         }
+    }
 
-        private void ClickB1()
-        {
-            b1++;
-        }
+    void clickb1()
+    {
+        B1++;
+    }
 
-        private void ClickB2()
-        {
-            b2++;
-        }
+    void clickb2()
+    {
+        B2++;
+    }
 
-        private void ClickB3()
-        {
-            b3++;
-        }
+    void clickb3()
+    {
+        B3++;
     }
 }
